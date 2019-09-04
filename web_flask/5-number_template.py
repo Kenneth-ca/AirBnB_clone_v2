43869cb1911a57e973162ec60ef06283da67673f
Kenneth-cad <kenneth.ca95@gmail.com>
2019-09-04 20:31:08 +0000

    How to use templates

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+from flask import Flask, render_template
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hello():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def hello_more(text):
+    a_string = 'C ' + text
+    return a_string.replace('_', ' ')
+
+
+@app.route('/python/')
+def hello_python():
+    a_string = 'Python ' + 'is cool'
+    return a_string.replace('_', ' ')
+
+
+@app.route('/python/<text>')
+def hello_python1(text):
+    a_string = 'Python ' + text
+    return a_string.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def hello_number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>')
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
