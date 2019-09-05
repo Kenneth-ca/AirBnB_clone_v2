e70097c33cf9a676598dbf29b7ecdf618eb6d1f9
Kenneth-cad <kenneth.ca95@gmail.com>
2019-09-05 00:36:13 +0000

    Update engines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,6 @@ class FileStorage:
             del_obj = type(obj).__name__ + "." + obj.id
             if del_obj in self.__objects:
                 self.__objects.pop(del_obj)
+
+    def close(self):
+        reload(self)
